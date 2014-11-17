6032ad77d1f171e20f478089a9695d36e2b83497
romaincucu <romain.cunault@gmail.com>
2014-11-17 21:16:56 +0100

    ajout post et call back

--- js/connected.js
+++ js/connected.js
@@ -21,6 +21,34 @@ index.on_click_function_ = function(ev){
 	}
 };
 
+
+index.post = function (data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/");
+    xhr.onreadystatechange = callback;
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify(data));
+};
+
+// fonction de retour pour notre objet index
+index.callback = function () {
+	// si tout s'est bien passé
+	if (this.readyState == 4 && this.status == 200) {		
+		console.log("this.responsetext :" + this.responseText);
+		var r = JSON.parse(this.responseText); // conversion string en Objet JSON
+		
+		if (r.message=="login_connexion_autorised_"){
+			window.location = "./html/connected.html";
+		}else if (r.message=="login_connexion_refused"){
+			document.getElementById(contenuHTML.id).innerHTML = contenuHTML.string;//pour remettre le bouton originel (car gif qui tourne)
+			index.mettre_les_cases_en_rouges_du_formulaire("boites_pour_entrer_les_login_");
+			alert("Erreur de connexion");
+		}else{
+			alert("demande  rejetée !");
+		}
+}
+};
+
 window.onload = function(){
 		setTimeout(index.start, 1);
 };
