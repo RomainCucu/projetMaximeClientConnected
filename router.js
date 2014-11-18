4e9d7f21cbba758975b1a961e030a402066d0c03
romaincucu <romain.cunault@gmail.com>
2014-11-18 01:43:09 +0100

    verif router si les champs du login contienne pas des choses bizarre

--- router.js
+++ router.js
@@ -101,8 +101,11 @@ go_post:
 		this.b = b;
 		if (b.ac == "check_login_process_") {
 			//this.resp.writeHead(200,{"Content-Type": "application/json" }); en commentaire car pas présent dans le router du projet ligotdor
-			db.login(b.userName, b.password, this.resp);
-			console.log("ENVOIE D'UNE DEMANDE DE LOGIN");
+			if (verification_data_entrantes.check_register_ingo(b)){//voir si objet contient pas de caract spéciaux, espace, et longueur entre 3 et 10
+				db.login(b.userName, b.password, this.resp);
+			}else{
+				this.resp.end(JSON.stringify({message: "else"}));
+			}			
 		}
 		
 		else if (b.ac == "register"){
